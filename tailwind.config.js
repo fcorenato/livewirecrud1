11901b113ca962dcbfe3bab584ad10dca636f02c
Renato Gomes <fco.renatogomes@gmail.com>
2024-05-16 15:42:07 -0300

    instaldo o tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./app/View/**/*.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    './resources/**/*.jpg',
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
